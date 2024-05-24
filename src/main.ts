b2ec774789aaa2701ed5db2eed324f4a47c5d482
hermesalvesbr <hermes.alves@araripina.com.br>
2024-05-24 16:11:55 -0300

    feat: now swagger on it

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
-import fastifySwagger from '@fastify/swagger'
-import fastifySwaggerUi from '@fastify/swagger-ui'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,43 +12,14 @@ async function bootstrap() {
     new FastifyAdapter({ logger: true }),
   )
 
-  // Configurar Swagger
-  app.register(fastifySwagger, {
-    swagger: {
-      info: {
-        title: 'API Documentation',
-        description: 'The API description',
-        version: '1.0',
-      },
-      tags: [{ name: 'example', description: 'Example operations' }],
-      schemes: ['http'],
-      consumes: ['application/json'],
-      produces: ['application/json'],
-    },
-    exposeRoute: true,
-  })
-
-  app.register(fastifySwaggerUi, {
-    routePrefix: '/documentation',
-    uiConfig: {
-      docExpansion: 'full',
-      deepLinking: false,
-    },
-    uiHooks: {
-      onRequest: function (request, reply, next) {
-        next()
-      },
-      preHandler: function (request, reply, next) {
-        next()
-      },
-    },
-    staticCSP: true,
-    transformStaticCSP: (header) => header,
-    transformSpecification: (swaggerObject, request, reply) => {
-      return swaggerObject
-    },
-    transformSpecificationClone: true,
-  })
+  const config = new DocumentBuilder()
+    .setTitle('EduPrime Core')
+    .setDescription('Api logical core for EduPrime project')
+    .setVersion('0.1')
+    .addTag('EduPrime')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('', app, document)
 
   await app.listen(3000, '0.0.0.0')
 }
